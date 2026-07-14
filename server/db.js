b3bd8ef3765afb4af039221b814ad910d53770bd
DdxAhmed <chessahmed21@gmail.com>
2026-07-14 21:29:19 +0300

    support loading .env from parent directory in db.js

--- server/db.js
+++ server/db.js
@@ -1,7 +1,15 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Load environment variables from the current directory or the parent directory
 dotenv.config();
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
 const MONGODB_URI = process.env.MONGODB_URI || process.env.MONGO_URI;
 
